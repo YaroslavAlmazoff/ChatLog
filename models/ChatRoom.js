1c255d6376e2c3862a80b3c31af3ec86516c33a8
yaroslav <yaroslav.almazoff@mail.ru>
2023-06-24 16:31:49 +0300

    15322

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ChatRoom = new mongoose.Schema({
-    creator: {type: mongoose.Types.ObjectId},
-    title: {type: String, default: 'Новая беседа'},
-    avatarUrl: {type: String, default: ''},
-    bg: {type: String},
-    members: [{type: mongoose.Types.ObjectId, required: true, default: []}],
-    lastMessage: {type: String, default: ''},
-})
+  creator: { type: mongoose.Types.ObjectId },
+  title: { type: String, default: "Новая беседа" },
+  avatarUrl: { type: String, default: "" },
+  bg: { type: String, default: "" },
+  members: [{ type: mongoose.Types.ObjectId, required: true, default: [] }],
+  lastMessage: { type: String, default: "" },
+});
 
-module.exports = mongoose.model('ChatRoom', ChatRoom)
+module.exports = mongoose.model("ChatRoom", ChatRoom);
