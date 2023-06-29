fea2154672506de82ebed4e2e96d8e9b1cbd413c
yaroslav <yaroslav.almazoff@mail.ru>
2023-06-29 14:43:46 +0300

    27974

--- client/src/file_storage/components/parts/ContextMenu.js
+++ client/src/file_storage/components/parts/ContextMenu.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContextMenu = ({ x, y, onClose, children }) => {
   return (
-    <ul className="context-menu" style={{ left: x, top: y }}>
+    <ul className="context-menu block" style={{ left: x, top: y }}>
       {children}
     </ul>
   );
