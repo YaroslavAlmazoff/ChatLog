0363aade2e28e7a49fe1910b2d58275808342673
yaroslav <yaroslav.almazoff@mail.ru>
2023-07-01 18:29:32 +0300

    25348

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,64 +1,94 @@
-import React, { useContext, useState, useRef } from 'react'
-import "./styles/form.css"
-import api from "./api/auth"
-import {AuthContext} from '../context/AuthContext'
-import Notice from './parts/Notice'
-import Loader from '../common_components/Loader'
+import React, { useContext, useState, useRef } from "react";
+import "./styles/form.css";
+import api from "./api/auth";
+import { AuthContext } from "../context/AuthContext";
+import Notice from "./parts/Notice";
+import Loader from "../common_components/Loader";
 
 const Login = () => {
-    /*let ref1 = useRef(null)
+  /*let ref1 = useRef(null)
     let ref2 = useRef(null)
     let ref3 = useRef(null)
     let ref4 = useRef(null)*/
-    const [noticeText, setNoticeText] = useState('')
-    const [noticeDisplay, setNoticeDisplay] = useState('none')
-    const [loading, setLoading] = useState(false)
-    const noticeRef = useRef(null)
-    //Страница логина
-    const auth = useContext(AuthContext)
-    //Получение функции навигации
-    //Инициализация состояний электронной почты и пароля пользователя
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    //Вход пользователя
-    const loginHandler = async () => {
-        setLoading(true)
-        //Создание объекта для отправки на сервер
-        const user = {
-            email, password
-        }
-        //Отправка запроса на вход пользователя
-        const response = await api.post('/api/auth/login', user)
-        auth.login(response.data.token, response.data.userId)
-        setLoading(false)
-        //Запись в локальном хранилище браузера ID пользователя
-        if(response.data.token && response.data.userId) {
-            localStorage.setItem('adblock', false)
-            window.location = `/home`
-        } else {
-            setNoticeText('Введены некорректные данные')
-            setNoticeDisplay('block')
-        }
-        
+  const [noticeText, setNoticeText] = useState("");
+  const [noticeDisplay, setNoticeDisplay] = useState("none");
+  const [loading, setLoading] = useState(false);
+  const noticeRef = useRef(null);
+  //Страница логина
+  const auth = useContext(AuthContext);
+  //Получение функции навигации
+  //Инициализация состояний электронной почты и пароля пользователя
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //Вход пользователя
+  const loginHandler = async () => {
+    setLoading(true);
+    //Создание объекта для отправки на сервер
+    const user = {
+      email,
+      password,
+    };
+    //Отправка запроса на вход пользователя
+    const response = await api.post("/api/auth/login", user);
+    console.log(response.data.token, response.data.userId);
+    auth.login(response.data.token, response.data.userId);
+    setLoading(false);
+    //Запись в локальном хранилище браузера ID пользователя
+    if (response.data.token && response.data.userId) {
+      localStorage.setItem("adblock", false);
+      window.location = `/home`;
+    } else {
+      setNoticeText("Введены некорректные данные");
+      setNoticeDisplay("block");
     }
-    // const theme = (theme, ref, num) => {
-    //     localStorage.setItem('theme', theme)
-    //     ref.current.className = 'theme-button-wb'
-    //     ref.current.classList.add('theme-button-bg'+num)
-    // }
-    return (
-        <div>
-            <Notice noticeText={noticeText} noticeDisplay={noticeDisplay} noticeRef={noticeRef} />
-            <div className="form">
-                <h2 className="white-glow-text">Вход</h2>
-                {!loading ? <><input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Введите свой email" type="email" className="input" />
-                <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Введите пароль" type="password" className="input" />
-                {/*localStorage.getItem('adblock') ? <p style={{color: 'yellow', fontSize: '16pt'}}>Если у вас стоит блокировщик рекламы, пожалуйста, отключите его на этом сайте. Он мешает корректной работе соцсети.</p>: <></>*/}
-                <button onClick={loginHandler} className="button">Войти</button></> : <><p style={{color: 'white'}}>Подождите, пока данные отправятся на сервер...</p><br /><Loader ml={'50%'} /></>}
-            </div>
-        </div>
-        
-    )
-}
+  };
+  // const theme = (theme, ref, num) => {
+  //     localStorage.setItem('theme', theme)
+  //     ref.current.className = 'theme-button-wb'
+  //     ref.current.classList.add('theme-button-bg'+num)
+  // }
+  return (
+    <div>
+      <Notice
+        noticeText={noticeText}
+        noticeDisplay={noticeDisplay}
+        noticeRef={noticeRef}
+      />
+      <div className="form">
+        <h2 className="white-glow-text">Вход</h2>
+        {!loading ? (
+          <>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Введите свой email"
+              type="email"
+              className="input"
+            />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Введите пароль"
+              type="password"
+              className="input"
+            />
+            {/*localStorage.getItem('adblock') ? <p style={{color: 'yellow', fontSize: '16pt'}}>Если у вас стоит блокировщик рекламы, пожалуйста, отключите его на этом сайте. Он мешает корректной работе соцсети.</p>: <></>*/}
+            <button onClick={loginHandler} className="button">
+              Войти
+            </button>
+          </>
+        ) : (
+          <>
+            <p style={{ color: "white" }}>
+              Подождите, пока данные отправятся на сервер...
+            </p>
+            <br />
+            <Loader ml={"50%"} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/homepage/components/Head.js
+++ client/src/homepage/components/Head.js
@@ -1,55 +1,68 @@
-import './styles/head.css'
-import { useRef, useEffect, useState, useContext } from "react"
-import useDaytime from '../hooks/daytime.hook'
-import api from '../../auth/api/auth'
-import { AuthContext } from '../../context/AuthContext'
+import "./styles/head.css";
+import { useRef, useEffect, useState, useContext } from "react";
+import useDaytime from "../hooks/daytime.hook";
+import api from "../../auth/api/auth";
+import { AuthContext } from "../../context/AuthContext";
 
 const Head = () => {
-    const auth = useContext(AuthContext)
-    const {getDaytime} = useDaytime()
-    const [user, setUser] = useState({})
+  const auth = useContext(AuthContext);
+  const { getDaytime } = useDaytime();
+  const [user, setUser] = useState({ name: "name" });
 
-    let clockRef = useRef(null)
-    useEffect(() => {
-            setInterval(() => {
-                 let now = new Date()
-                 clockRef.current.innerHTML = now.toLocaleTimeString()
-            }, 1000)
-    }, [])
+  let clockRef = useRef(null);
+  useEffect(() => {
+    setInterval(() => {
+      let now = new Date();
+      clockRef.current.innerHTML = now.toLocaleTimeString();
+    }, 1000);
+  }, []);
 
-    useEffect(() => {
-        const getUser = async () => {
-            const response = await api.get('/api/user', {headers: {
-                Authorization: `Bearer ${auth.token}`
-            }})
-            setUser(response.data.user)
-        }
-        getUser()
-    }, [auth])
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await api.get("/api/user", {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      });
+      setUser(response.data.user);
+    };
+    getUser();
+  }, [auth]);
 
-    return (
-        <div className="head">
-            {window.screen.width > 700 
-            ? <div className='daytime'>
-            <h2 className='time'>{user.name ? getDaytime(user.name) : ''}&nbsp;&nbsp;&nbsp;&nbsp;</h2>
-            <div ref={clockRef} className="time"></div>
-        </div> : <></>
-            }
-            {window.screen.width < 700 && window.screen.width > 500
-            ? <div className='daytime'>
-            <p className='time'>{user.name ? getDaytime(user.name) : ''}&nbsp;&nbsp;&nbsp;&nbsp;</p>
-            <div ref={clockRef} className="time"></div>
-        </div> : <></>
-            }
-            {window.screen.width < 500 
-            ? <div className='daytime'>
-            <p className='time white-glow-text'>{user.name ? getDaytime(user.name) : ''}</p>
-            <div ref={clockRef} className="time white-glow-text"></div>
-        </div> : <></>
-            }
-            
+  return (
+    <div className="head">
+      {window.screen.width > 700 ? (
+        <div className="daytime">
+          <h2 className="time">
+            {user.name ? getDaytime(user.name) : ""}&nbsp;&nbsp;&nbsp;&nbsp;
+          </h2>
+          <div ref={clockRef} className="time"></div>
         </div>
-    )
-}
+      ) : (
+        <></>
+      )}
+      {window.screen.width < 700 && window.screen.width > 500 ? (
+        <div className="daytime">
+          <p className="time">
+            {user.name ? getDaytime(user.name) : ""}&nbsp;&nbsp;&nbsp;&nbsp;
+          </p>
+          <div ref={clockRef} className="time"></div>
+        </div>
+      ) : (
+        <></>
+      )}
+      {window.screen.width < 500 ? (
+        <div className="daytime">
+          <p className="time white-glow-text">
+            {user.name ? getDaytime(user.name) : ""}
+          </p>
+          <div ref={clockRef} className="time white-glow-text"></div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
-export default Head
+export default Head;
