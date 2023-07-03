e606ba17509641d5568b9cab33424edd1e237a83
yaroslav <yaroslav.almazoff@mail.ru>
2023-07-03 20:40:32 +0300

    25253

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import $ from "jquery";
 
 const colors = [
   [15, 15, 40],
