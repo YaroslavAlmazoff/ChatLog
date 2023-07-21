bdf85eda006539e5b423cdb44252748c10c2796c
yaroslav <yaroslav.almazoff@mail.ru>
2023-07-21 09:56:09 +0300

    10047

--- services/ExcelService.js
+++ services/ExcelService.js
@@ -2,10 +2,32 @@ const XLSX = require("xlsx");
 const fs = require("fs");
 
 class ExcelService {
+  convertNumbersToStrings(obj) {
+    if (typeof obj === "object") {
+      for (let key in obj) {
+        if (typeof obj[key] === "number") {
+          obj[key] = obj[key].toString();
+        } else if (typeof obj[key] === "object") {
+          convertNumbersToStrings(obj[key]);
+        }
+      }
+    } else {
+      obj = JSON.parse(obj);
+      for (let key in obj) {
+        if (typeof obj[key] === "number") {
+          obj[key] = obj[key].toString();
+        } else if (typeof obj[key] === "object") {
+          convertNumbersToStrings(obj[key]);
+        }
+      }
+    }
+  }
   toJSON(path) {
     const workbook = XLSX.readFile(path);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+    const jsonData = this.convertNumbersToStrings(
+      XLSX.utils.sheet_to_json(worksheet)
+    );
     console.log(jsonData);
     return jsonData;
   }
