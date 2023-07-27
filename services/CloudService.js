22d121fb808da886043fb3c9b30fc5b9950621c1
yaroslav <yaroslav.almazoff@mail.ru>
2023-07-27 21:14:27 +0300

    16379

--- services/CloudService.js
+++ services/CloudService.js
@@ -187,7 +187,7 @@ class CloudService {
     console.log(req.files);
     let folder;
     if (req.body.mobile) {
-      folder = this.removeBackslash(req.body.folder);
+      folder = JSON.parse(this.removeBackslash(req.body.folder));
     } else {
       folder = JSON.parse(req.body.folder);
       names = JSON.parse(req.body.names);
