bc2c0590d6f7c42ea06c305d37d3f6850f9db239
yaroslav <yaroslav.almazoff@mail.ru>
2023-08-02 07:09:32 +0300

    21870

--- client/src/publics/components/pages/PublicsPage.js
+++ client/src/publics/components/pages/PublicsPage.js
@@ -1,49 +1,74 @@
-import { useEffect, useState } from "react"
-import api from '../../../auth/api/auth'
-import Public from "../components/components/Public"
-import '../../styles/publics.css'
-import ShowAdLeft from "../../../inner_ad/components/components/ShowAdLeft"
-import UsersSearchSide from "../components/UsersSearchSide"
-import { useMemo } from "react"
-import Loader from "../../../common_components/Loader"
+import { useContext, useEffect, useState } from "react";
+import api from "../../../auth/api/auth";
+import Public from "../components/components/Public";
+import "../../styles/publics.css";
+import ShowAdLeft from "../../../inner_ad/components/components/ShowAdLeft";
+import UsersSearchSide from "../components/UsersSearchSide";
+import { useMemo } from "react";
+import Loader from "../../../common_components/Loader";
+import { AuthContext } from "../../../context/AuthContext";
 
 const PublicsPage = () => {
-    const [publics, setPublics] = useState([])
-    const [searchValue, setSearchValue] = useState('')
-    const [loading, setLoading] = useState(true)
+  const auth = useContext(AuthContext);
+  const [publics, setPublics] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const getPublics = async () => {
-            const response = await api.get('/api/public/all')
-            setPublics(response.data.publics)
-            setLoading(false)
-        }
-        getPublics()
-    }, [])
+  useEffect(() => {
+    const getPublics = async () => {
+      const response = await api.get(`/api/public/all/${auth.userId}`);
+      setPublics(response.data.publics);
+      setLoading(false);
+    };
+    getPublics();
+  }, []);
 
-    const createPublic = () => {
-        window.location = '/createpublic'
-    }
+  const createPublic = () => {
+    window.location = "/createpublic";
+  };
 
-    const searchedPublics = useMemo(() => {
-        return [...publics].filter(el => {
-            return el.name.toLowerCase().includes(searchValue.toLowerCase()) || 
-            el.description.toLowerCase().includes(searchValue.toLowerCase()) ||
-            searchValue === ''
-        })
-    }, [publics, searchValue])
+  const searchedPublics = useMemo(() => {
+    return [...publics].filter((el) => {
+      return (
+        el.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        el.description.toLowerCase().includes(searchValue.toLowerCase()) ||
+        searchValue === ""
+      );
+    });
+  }, [publics, searchValue]);
 
-    return (
-        <div style={{display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-            <div className="users-ads">
-                <UsersSearchSide searchValue={searchValue} setSearchValue={setSearchValue} />  
-            </div>
-            {!loading ? <div className="publics">
-                <button onClick={createPublic} className="button" style={{marginBottom: '15px'}}>Создать группу</button>
-                {searchedPublics.map(item => <Public item={item} />)}
-            </div> : <Loader ml={'50%'} />}
-        </div>    
-    )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+      }}
+    >
+      <div className="users-ads">
+        <UsersSearchSide
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </div>
+      {!loading ? (
+        <div className="publics">
+          <button
+            onClick={createPublic}
+            className="button"
+            style={{ marginBottom: "15px" }}
+          >
+            Создать группу
+          </button>
+          {searchedPublics.map((item) => (
+            <Public item={item} />
+          ))}
+        </div>
+      ) : (
+        <Loader ml={"50%"} />
+      )}
+    </div>
+  );
+};
 
-export default PublicsPage
+export default PublicsPage;
