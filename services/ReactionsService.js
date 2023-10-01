04ccfa3f9bf1deabf636566b39d64b788cd7a33d
yaroslav <yaroslav.almazoff@mail.ru>
2023-10-01 13:46:16 +0300

    5631

--- services/ReactionsService.js
+++ services/ReactionsService.js
@@ -148,7 +148,7 @@ class ReactionService {
         click_action: "POST",
       });
       FirebaseService.send(text, "", token.token, {
-        id: post._id.toString(),
+        id: user._id.toString(),
         type: "comment",
         message: text,
         name: "",
