03d01748873b112221fd78cef1853031bf25cb98
yaroslav <yaroslav.almazoff@mail.ru>
2024-01-01 15:26:55 +0300

    26973

--- services/Astronomical/AEPController.js
+++ services/Astronomical/AEPController.js
@@ -81,6 +81,33 @@ class AEPController {
       const tokens = await AEPNotificationToken.find({});
       let date = new Date();
       for (var item of events) {
+        console.log(
+          item.year,
+          date.getFullYear(),
+          item.year <= date.getFullYear()
+        );
+        console.log(
+          item.month,
+          getMonth().string,
+          item.month == getMonth().string
+        );
+        console.log(
+          Number(item.time.split(":")[0]),
+          date.getHours(),
+          Number(item.time.split(":")[0]) == date.getHours()
+        );
+        console.log(
+          Number(item.time.split(":")[1]),
+          date.getMinutes(),
+          Number(item.time.split(":")[1]) <= date.getMinutes()
+        );
+        console.log(
+          item.year <= date.getFullYear() &&
+            item.month == getMonth().string &&
+            item.day == date.getDate() &&
+            Number(item.time.split(":")[0]) == date.getHours() &&
+            Number(item.time.split(":")[1]) <= date.getMinutes()
+        );
         if (
           item.year <= date.getFullYear() &&
           item.month == getMonth().string &&
