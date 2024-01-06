2d8b16e8df8d439790abb0a556b121756cb96de4
yaroslav <yaroslav.almazoff@mail.ru>
2024-01-06 20:57:48 +0300

    24971

--- server.js
+++ server.js
@@ -91,9 +91,9 @@ app.get("/serviceworker.js", (req, res) => {
 app.get("/manifest.json", (req, res) => {
   res.sendFile(path.resolve(__dirname, "manifest.json"));
 });
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 app.get("/art/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "site_art", "build", "index.html"));
 });
