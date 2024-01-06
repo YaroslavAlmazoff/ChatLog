b78f7a51507dd46ed874602b55f19c93de4e37be
yaroslav <yaroslav.almazoff@mail.ru>
2024-01-06 20:50:19 +0300

    6296

--- site_art/src/App.js
+++ site_art/src/App.js
@@ -1,34 +1,43 @@
-import { Route, Routes } from 'react-router';
-import './App.css';
-import Header from './Header';
-import Contacts from './Contacts';
-import Main from './Main';
-import Buy from './Buy';
-import AboutAuthor from './AboutAuthor';
-import Reviews from './Reviews';
-import Answer from './Answer';
-import Footer from './Footer';
-import Picture from './Picture';
-import { useState } from 'react';
-import Zakaz from './Zakaz';
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Header from "./Header";
+import Contacts from "./Contacts";
+import Main from "./Main";
+import Buy from "./Buy";
+import AboutAuthor from "./AboutAuthor";
+import Reviews from "./Reviews";
+import Answer from "./Answer";
+import Footer from "./Footer";
+import Picture from "./Picture";
+import { useState } from "react";
+import Zakaz from "./Zakaz";
 
 function App() {
-  const numberValue = localStorage.getItem("number") 
-  const [number, setNumber] = useState(numberValue != null ? numberValue : 0)
-  localStorage.setItem('basket', JSON.stringify([]))
-  localStorage.setItem('visited', true)
+  const numberValue = localStorage.getItem("number");
+  const [number, setNumber] = useState(numberValue != null ? numberValue : 0);
+  localStorage.setItem("basket", JSON.stringify([]));
+  localStorage.setItem("visited", true);
   return (
     <div className="page-center">
-      <Header number={number} /> 
-      <Routes> {/* Маршруты */}
-        <Route path="/" element={<Main number={number} setNumber={setNumber} />}/>
-        <Route path="/contacts" element={<Contacts />}/>
-        <Route path="/buy" element={<Buy />}/>
-        <Route path="/about-author" element={<AboutAuthor />}/>
-        <Route path="/reviews" element={<Reviews />}/>
-        <Route path="/answer" element={<Answer />}/>
-        <Route path="/picture/:id" element={<Picture number={number} setNumber={setNumber} />}/>
-        <Route path="/zakaz/:id/:i" element={<Zakaz />}/></Routes>
+      <Header number={number} />
+      <Routes>
+        {" "}
+        {/* Маршруты */}
+        <Route
+          path="/art/"
+          element={<Main number={number} setNumber={setNumber} />}
+        />
+        <Route path="/art/contacts" element={<Contacts />} />
+        <Route path="/art/buy" element={<Buy />} />
+        <Route path="/art/about-author" element={<AboutAuthor />} />
+        <Route path="/art/reviews" element={<Reviews />} />
+        <Route path="/art/answer" element={<Answer />} />
+        <Route
+          path="/art/picture/:id"
+          element={<Picture number={number} setNumber={setNumber} />}
+        />
+        <Route path="/art/zakaz/:id/:i" element={<Zakaz />} />
+      </Routes>
       <Footer />
     </div>
   );
