5ec60e80a44c0f55b71c1654883292599888158d
yaroslav <yaroslav.almazoff@mail.ru>
2024-01-09 00:14:30 +0300

    26540

--- client/src/site_art/AboutAuthor.js
+++ client/src/site_art/AboutAuthor.js
@@ -1,4 +1,5 @@
 import "./styles/AboutAuthor.css";
+import "./styles/Main.css";
 
 const isMobile =
   /Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iP(ad|od|hone)/i.test(
