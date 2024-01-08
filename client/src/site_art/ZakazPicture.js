43ecc384b2e2640df73f89b3e32a8b2cc21318d1
yaroslav <yaroslav.almazoff@mail.ru>
2024-01-08 13:12:04 +0300

    23301

--- client/src/site_art/ZakazPicture.js
+++ client/src/site_art/ZakazPicture.js
@@ -1,27 +1,28 @@
-import pictures from "./pictures"
-import "./styles/Buy.css"
+import pictures from "./pictures";
+import "./styles/Buy.css";
 
-const ZakazPicture = ({index}) => {
+const ZakazPicture = ({ index }) => {
+  const picture = pictures[index];
 
-    const picture = pictures[index]
+  return (
+    <div className="zakaz-picture-parent">
+      <div className="zakaz-picture">
+        <img
+          src={process.env.REACT_APP_API_URL + "/" + picture.image}
+          className="zakaz-image"
+        />
+      </div>
+      <span style={{ width: "250%" }}>
+        <p className="buy-text2">Картина: ‹‹{picture.title}››</p>
+        <p className="buy-text2">{picture.technic}</p>
+        <p className="buy-text2">{picture.size}</p>
+        <br />
+        <br />
+        <br />
+        <p className="buy-text2">Итого: {picture.price}₽</p>
+      </span>
+    </div>
+  );
+};
 
-    return (
-        <div className="zakaz-picture-parent">
-            <div className="zakaz-picture">
-                <img src={picture.image} className="zakaz-image" />
-            </div>
-            <span style={{width: "250%"}}>
-                <p className="buy-text2">Картина: ‹‹{picture.title}››</p>
-                <p className="buy-text2">{picture.technic}</p>
-                <p className="buy-text2">{picture.size}</p>
-                <br />
-                <br />
-                <br />
-                <p className="buy-text2">Итого: {picture.price}₽</p>
-            </span>
-            
-        </div>
-    )
-}
-
-export default ZakazPicture
+export default ZakazPicture;
