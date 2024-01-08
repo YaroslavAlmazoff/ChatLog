4ea501f15184b3e5ec25b579a0091deb3e51f000
yaroslav <yaroslav.almazoff@mail.ru>
2024-01-08 13:25:09 +0300

    14370

--- client/src/site_art/Zakaz.js
+++ client/src/site_art/Zakaz.js
@@ -1,26 +1,26 @@
-import { useParams } from "react-router"
-import ZakazDelivery from "./ZakazDelivery"
-import ZakazForm from "./ZakazForm"
-import ZakazPicture from "./ZakazPicture"
-import "./styles/Zakaz.css"
-import "./styles/Buy.css"
-import pictures from "./pictures"
-
-
+import { useParams } from "react-router";
+import ZakazDelivery from "./ZakazDelivery";
+import ZakazPicture from "./ZakazPicture";
+import "./styles/Zakaz.css";
+import "./styles/Buy.css";
+import pictures from "./pictures";
 
 const Zakaz = () => {
-    const params = useParams()
-    const picture = pictures[params.i]
+  const params = useParams();
+  const picture = pictures[params.i];
 
-    return (
-        <div className="zakaz" style={{color: "white", textDecoration: 'underline'}}>
-            <p className="zakaz-text">Оформление заказа</p>
-            <ZakazPicture index={params.i} />
-            <div className="zakaz-bottom">
-                <ZakazDelivery picture={picture.title} price={picture.price} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className="zakaz"
+      style={{ color: "white", textDecoration: "underline" }}
+    >
+      <p className="zakaz-text">Оформление заказа</p>
+      <ZakazPicture index={params.i} />
+      <div className="zakaz-bottom">
+        <ZakazDelivery picture={picture.title} price={picture.price} />
+      </div>
+    </div>
+  );
+};
 
-export default Zakaz
+export default Zakaz;
