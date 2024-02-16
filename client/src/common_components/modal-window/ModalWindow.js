37b30bf7247d7868c5e384d097b9e4bc9883ce7a
yaroslav <yaroslav.almazoff@mail.ru>
2024-02-16 20:15:37 +0300

    31962

--- client/src/common_components/modal-window/ModalWindow.js
+++ client/src/common_components/modal-window/ModalWindow.js
@@ -7,20 +7,18 @@ const ModalWindow = ({ isOpen, onClose, onConfirm, text }) => {
   }
 
   return (
-    <div className={`modal-overlay ${isOpen ? "open" : ""}`}>
-      <div className="modal-window-container block">
-        <div className="modal-window-content">
-          <p className="modal-window-text">{text}</p>
-          <button
-            className="modal-window-button button-neon-red"
-            onClick={onConfirm}
-          >
-            Подтвердить
-          </button>
-          <span className="modal-window-cancel" onClick={onClose}>
-            Отмена
-          </span>
-        </div>
+    <div className="modal-window-container block">
+      <div className="modal-window-content">
+        <p className="modal-window-text">{text}</p>
+        <button
+          className="modal-window-button button-neon-red"
+          onClick={onConfirm}
+        >
+          Подтвердить
+        </button>
+        <span className="modal-window-cancel" onClick={onClose}>
+          Отмена
+        </span>
       </div>
     </div>
   );
