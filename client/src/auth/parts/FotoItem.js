b31567628854efd28f3acbaa1be7b737bed844eb
yaroslav <yaroslav.almazoff@mail.ru>
2024-02-17 11:38:23 +0300

    27769

--- client/src/auth/parts/FotoItem.js
+++ client/src/auth/parts/FotoItem.js
@@ -1,38 +1,13 @@
-import { AuthContext } from "../../context/AuthContext";
-import ModalWindow from "../../common_components/modal-window/ModalWindow";
-import { useState, useContext } from "react";
-
 const FotoItem = ({ item, deleteFoto, isOwner }) => {
-  const auth = useContext(AuthContext);
-  const [modal, setModal] = useState(false);
-  const openWindow = () => {
-    setModal(true);
-    auth.darkScreen(true);
-  };
-  const closeWindow = () => {
-    setModal(false);
-    auth.darkScreen(false);
-  };
-  const onConfirm = () => {
-    deleteFoto(item.imageUrl);
-  };
   return (
     <div className="foto-div">
-      {modal && (
-        <ModalWindow
-          isOpen={modal}
-          onClose={closeWindow}
-          onConfirm={onConfirm}
-          text="Вы действительно хотите удалить эту фотографию?"
-        />
-      )}
       <img
         className="user-foto-preview block"
         alt=""
         src={process.env.REACT_APP_API_URL + "/userfotos/" + item.imageUrl}
       />
       {isOwner && (
-        <span className="delete-foto" onClick={openWindow}>
+        <span className="delete-foto" onClick={() => deleteFoto(item.imageUrl)}>
           Удалить
         </span>
       )}
