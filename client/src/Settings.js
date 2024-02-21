4ad1545ce0045720b3bbd1848672e3355608e294
yaroslav <yaroslav.almazoff@mail.ru>
2024-02-21 16:41:57 +0300

    7123

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -1,32 +1,36 @@
-import useVerify from "./common_hooks/verify.hook"
-import { useEffect } from "react"
-import './settings.css'
+import useVerify from "./common_hooks/verify.hook";
+import { useEffect } from "react";
+import "./settings.css";
 
 const Settings = () => {
-    const {verify} = useVerify()
-    useEffect(() => {
-        verify()
-    } ,[])
-    const logout = () => {
-        localStorage.removeItem("user")
-        window.location = "/"
-    }
-    const deleteProfile = () => {
-        window.location = "/deleteprofile"
-    }
+  const { verify } = useVerify();
+  useEffect(() => {
+    verify();
+  }, []);
+  const logout = () => {
+    localStorage.removeItem("user");
+    window.location = "/";
+  };
+  const deleteProfile = () => {
+    window.location = "/deleteprofile";
+  };
 
-    return (
-        <div className="settings-wrapper">
-            <div className="settings">
-                <div className="settings-item">
-                    <button onClick={logout} className="button">Выйти</button>
-                </div>
-                <div className="settings-item">
-                    <button onClick={deleteProfile} className="button">Удалить профиль</button>
-                </div>
-            </div>
+  return (
+    <div className="settings-wrapper">
+      <div className="settings block">
+        <div className="settings-item">
+          <button onClick={logout} className="button">
+            Выйти
+          </button>
         </div>
-    )
-}
+        <div className="settings-item">
+          <button onClick={deleteProfile} className="button">
+            Удалить профиль
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Settings
+export default Settings;
