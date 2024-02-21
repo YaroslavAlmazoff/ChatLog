5d9c81ca1eea31bf5ebb7aba21b5335256416302
yaroslav <yaroslav.almazoff@mail.ru>
2024-02-21 17:43:53 +0300

    11185

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -21,16 +21,12 @@ const Settings = () => {
       <div className="settings block">
         <h2 className="form-title">Настройки</h2>
         <div className="setting-buttons">
-          <div className="settings-item">
-            <button onClick={logout} className="button">
-              Выйти
-            </button>
-          </div>
-          <div className="settings-item">
-            <button onClick={deleteProfile} className="button">
-              Удалить профиль
-            </button>
-          </div>
+          <button onClick={logout} className="button">
+            Выйти
+          </button>
+          <button onClick={deleteProfile} className="button">
+            Удалить профиль
+          </button>
         </div>
       </div>
     </div>
