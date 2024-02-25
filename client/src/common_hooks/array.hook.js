9ccf8730e77f8c88b4e57756362ba9f318ed2bcb
yaroslav <yaroslav.almazoff@mail.ru>
2024-02-25 09:45:06 +0300

    25032

--- client/src/common_hooks/array.hook.js
+++ client/src/common_hooks/array.hook.js
@@ -2,7 +2,7 @@ const useArray = () => {
   const unique = (a) => [...new Set(a)];
   const uniqueObjects = (array) =>
     array.filter(
-      (obj, index, self) => index === self.findIndex((o) => o.id === obj.id)
+      (obj, index, self) => index === self.findIndex((o) => o._id === obj._id)
     );
   return { unique, uniqueObjects };
 };
