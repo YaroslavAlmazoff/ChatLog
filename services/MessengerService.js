2281f41cf790e2ac22d10a46786cf33e7632e32f
yaroslav <yaroslav.almazoff@mail.ru>
2024-03-17 19:52:47 +0300

    31767

--- services/MessengerService.js
+++ services/MessengerService.js
@@ -158,9 +158,9 @@ class MessengerService {
             const filteredRooms = sortedRooms.filter((el) => el != null);
             res.json({ rooms: filteredRooms });
           })
-          .catch((e) => res.json({ e: e.message, rooms: [] }));
+          .catch((e) => res.json({ e: e.message, rooms: [], fullRooms }));
       })
-      .catch((e) => res.json({ e: e.message, rooms: [] }));
+      .catch((e) => res.json({ e: e.message, rooms: [], fullRooms }));
   }
   async getNotReadedRooms(req, res) {
     const user = req.user.userId;
