d7ada8cb658f4f7662a02e3aac67ec400352b096
yaroslav <yaroslav.almazoff@mail.ru>
2024-03-17 21:00:26 +0300

    24694

--- client/src/messenger/components/pages/RoomsList.js
+++ client/src/messenger/components/pages/RoomsList.js
@@ -44,7 +44,7 @@ export const RoomsList = () => {
         <button onClick={createChat} className="button">
           + Создать беседу
         </button>
-        <div className="rooms-list-wrapper">
+        <div className="rooms-list-wrapper mt">
           {!loading ? (
             rooms.map((el) => <RoomItem key={randomKey()} room={el} />)
           ) : (
