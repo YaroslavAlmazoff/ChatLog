c96760a9d142649362c98f577be0edef9451af52
yaroslav <yaroslav.almazoff@mail.ru>
2024-03-25 10:55:12 +0300

    28413

--- client/src/auth/News/components/components/PublicNewsPost.js
+++ client/src/auth/News/components/components/PublicNewsPost.js
@@ -54,6 +54,7 @@ const PublicNewsPost = ({ id }) => {
       const response = await api.get(`/api/public/comments/${post._id}`);
       console.log(response);
       setComments(response.data.comments.reverse());
+      setCommentsDisplay(true);
     };
     getComments();
     const checkLike = async () => {
