20c04071e9dfb8a71b85dec3af7f73fe197691d1
yaroslav <yaroslav.almazoff@mail.ru>
2024-03-26 08:04:39 +0300

    8421

--- client/src/publics/components/pages/PublicsPage.js
+++ client/src/publics/components/pages/PublicsPage.js
@@ -2,23 +2,28 @@ import { useContext, useEffect, useState } from "react";
 import api from "../../../auth/api/auth";
 import Public from "../components/components/Public";
 import "../../styles/publics.css";
+import "../../../auth/styles/users.css";
 import ShowAdLeft from "../../../inner_ad/components/components/ShowAdLeft";
 import UsersSearchSide from "../components/UsersSearchSide";
 import { useMemo } from "react";
 import Loader from "../../../common_components/Loader";
 import { AuthContext } from "../../../context/AuthContext";
+import useHighlight from "../../../common_hooks/highlight.hook";
 
 const PublicsPage = () => {
-  const auth = useContext(AuthContext);
+  const { randomBlockShadow } = useHighlight();
   const [publics, setPublics] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [searchDisplay, setSearchDisplay] = useState(false);
 
   useEffect(() => {
     const getPublics = async () => {
       const response = await api.get(`/api/public/publics`);
       setPublics(response.data.publics);
       setLoading(false);
+      setSearchDisplay(true);
     };
     getPublics();
   }, []);
@@ -38,28 +43,45 @@ const PublicsPage = () => {
   }, [publics, searchValue]);
 
   return (
-    <div className="publics center-column">
-      <div className="users-ads"></div>
-      {!loading ? (
-        <>
-          <button
-            onClick={createPublic}
-            className="button"
-            style={{ marginBottom: "15px" }}
-          >
-            Создать группу
-          </button>
-          <UsersSearchSide
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-          />
-          {searchedPublics.map((item) => (
-            <Public item={item} />
-          ))}
-        </>
-      ) : (
-        <Loader ml={"50%"} />
-      )}
+    <div className="users">
+      <div className="users-list">
+        {!loading ? (
+          <>
+            <button
+              onClick={createPublic}
+              className="button"
+              style={{ marginBottom: "15px" }}
+            >
+              Создать группу
+            </button>
+            <div className="users-search-side">
+              {searchDisplay && (
+                <div className={`search ${randomBlockShadow()}`}>
+                  <input
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    type="search"
+                    className="search-field"
+                    placeholder="Поиск..."
+                  />
+                  <button
+                    className="button"
+                    onClick={() => {
+                      setSearchValue(value);
+                      setPage(1);
+                    }}
+                  ></button>
+                </div>
+              )}
+            </div>
+            {searchedPublics.map((item) => (
+              <Public item={item} />
+            ))}
+          </>
+        ) : (
+          <Loader ml={"50%"} />
+        )}
+      </div>
     </div>
   );
 };
