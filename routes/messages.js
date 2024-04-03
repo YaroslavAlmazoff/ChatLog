f1a9e2026852372e06e7c5e8136dd0e58476b4dd
yaroslav <yaroslav.almazoff@mail.ru>
2024-04-03 12:53:50 +0300

    18036

--- routes/messages.js
+++ routes/messages.js
@@ -154,8 +154,8 @@ const filterMessages = async (room) => {
   );
 };
 
-const getMessagesPortion = (page) => {
-  const page = parseInt(page) || 1;
+const getMessagesPortion = (pageNumber) => {
+  const page = parseInt(pageNumber) || 1;
   const perPage = 10;
   const startIndex = (page - 1) * perPage;
   const endIndex = page * perPage;
