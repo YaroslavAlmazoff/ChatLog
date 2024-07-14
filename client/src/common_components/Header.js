bbf2ae207c49df5fbfc39a1c3268c9c274ee8c41
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-14 17:48:32 +0300

    24237

--- client/src/common_components/Header.js
+++ client/src/common_components/Header.js
@@ -3,29 +3,43 @@ import "./styles/header.css";
 import { useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import api from "../auth/api/auth";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const auth = useContext(AuthContext);
+  const authorizedRef = useRef();
+  const notAuthorizedRef = useRef();
 
   const isActivated = true;
 
-  //Компонент верхней части приложения
-  //Создание ссылок на ссылки:)
-  const linkRef1 = useRef("");
-  const linkRef2 = useRef("");
-  const linkRef3 = useRef("");
-  const linkRef4 = useRef("");
-  const linkRef5 = useRef("");
-  const linkRef6 = useRef("");
-  const linkRef7 = useRef("");
-  const linkRef8 = useRef("");
-  const linkRef9 = useRef("");
-  const linkRef10 = useRef("");
-  //Добавление классов ссылкам в зависимости от того на какой странице сейчас пользователь
-  const visit = (ref, link) => {
-    window.location = link;
-    ref.current.classList.add("link-visited");
-    ref.current.click();
+  const location = window.location.href;
+
+  console.log(location);
+
+  const [authorizedLinks, setAuthorizedLinks] = useState([
+    { name: "Главная", route: "/home", marked: this.route === location },
+    {
+      name: "Мой профиль",
+      route: "/user/" + auth.userId,
+      marked: this.route === location,
+    },
+    { name: "Сообщения", route: "/messages", marked: this.route === location },
+    { name: "Люди", route: "/users", marked: this.route === location },
+    { name: "Настройки", route: "/settings", marked: this.route === location },
+  ]);
+  const [notAuthorizedLinks, setNotAuthorizedLinks] = useState([
+    {
+      name: "Регистрация",
+      route: "/register",
+      marked: this.route === location,
+    },
+    { name: "Вход", route: "/login", marked: this.route === location },
+  ]);
+
+  const [activeLink, setActiveLink] = useState(null);
+
+  const markLink = (link) => {
+    setActiveLink(link);
   };
 
   return (
@@ -34,62 +48,41 @@ const Header = () => {
         <div className="header" id="header">
           <h2 className="logo">CHATLOG.RU</h2>
           <div className="links">
-            {auth.isAuthenticated ? (
+            {auth.authenticated ? (
               <div className="links">
-                <button
-                  ref={linkRef1}
-                  onClick={() => visit(linkRef1, "/home")}
-                  className="link"
-                >
-                  Главная
-                </button>
-                <button
-                  ref={linkRef8}
-                  onClick={() => visit(linkRef8, `/user/${auth.userId}`)}
-                  className="link"
-                >
-                  Мой профиль
-                </button>
-                <button
-                  ref={linkRef4}
-                  onClick={() => visit(linkRef4, "/messages")}
-                  className="link"
-                >
-                  Сообщения
-                </button>
-                <button
-                  ref={linkRef7}
-                  onClick={() => visit(linkRef7, "/users")}
-                  className="link"
-                  exact="true"
-                >
-                  Люди
-                </button>
-                <button
-                  ref={linkRef10}
-                  onClick={() => visit(linkRef10, "/settings")}
-                  className="link"
-                  exact="true"
-                >
-                  Настройки
-                </button>
+                {authorizedLinks.map((link) => (
+                  <Link
+                    onClick={markLink}
+                    ref={authorizedRef}
+                    to={link.route}
+                    className="link"
+                    style={
+                      activeLink === link.route
+                        ? { color: "white" }
+                        : { color: "black" }
+                    }
+                  >
+                    {link.name}
+                  </Link>
+                ))}
               </div>
             ) : (
               <div>
-                <button
-                  ref={linkRef5}
-                  onClick={() => visit(linkRef5, "/login")}
-                  className="link"
-                >
-                  Войти
-                </button>
-                <button
-                  ref={linkRef6}
-                  onClick={() => visit(linkRef6, "/register")}
-                  className="link"
-                >
-                  Регистрация
-                </button>
+                {notAuthorizedLinks.map((link) => (
+                  <Link
+                    onClick={markLink}
+                    ref={notAuthorizedRef}
+                    to={link.route}
+                    className="link"
+                    style={
+                      activeLink === link.route
+                        ? { color: "white" }
+                        : { color: "black" }
+                    }
+                  >
+                    {link.name}
+                  </Link>
+                ))}
               </div>
             )}
           </div>
