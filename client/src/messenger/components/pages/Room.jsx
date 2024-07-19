7099f2c73999443f7977fe2f0e08751d23f7ed9a
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-19 22:49:19 +0300

    6400

--- client/src/messenger/components/pages/Room.jsx
+++ client/src/messenger/components/pages/Room.jsx
@@ -4,7 +4,7 @@ import RoomMessageField from "../components/RoomMessageField";
 
 export default function Room() {
   return (
-    <div className="room-size room">
+    <div className="block room-size room">
       <RoomHead
         name="Yaroslav Almazoff"
         onlineDate="16.07.2024 18:00"
