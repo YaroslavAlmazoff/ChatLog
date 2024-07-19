707f51d1b7e34fe3ea449dadd5020e6adf814942
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-19 21:27:58 +0300

    17010

--- client/src/messenger/components/components/RoomImagePreview.jsx
+++ client/src/messenger/components/components/RoomImagePreview.jsx
@@ -1,6 +1,14 @@
 import useWindow from "../../hooks/useWindow";
+import "../../styles/RoomPreview.css";
 
 export default function RoomVideoPreview({ url }) {
   const { handleLoaded } = useWindow();
-  return <img src={url} alt="uploaded" onLoadedData={handleLoaded} />;
+  return (
+    <img
+      src={url}
+      alt="uploaded"
+      onLoadedData={handleLoaded}
+      className="room-preview"
+    />
+  );
 }
--- client/src/messenger/components/components/RoomVideoPreview.jsx
+++ client/src/messenger/components/components/RoomVideoPreview.jsx
@@ -1,6 +1,15 @@
 import useWindow from "../../hooks/useWindow";
+import "../../styles/RoomPreview.css";
 
 export default function RoomImagePreview({ url }) {
   const { handleLoaded } = useWindow();
-  return <video controls width="200" src={url} onLoadedData={handleLoaded} />;
+  return (
+    <video
+      controls
+      width="200"
+      src={url}
+      onLoadedData={handleLoaded}
+      className="room-preview"
+    />
+  );
 }
