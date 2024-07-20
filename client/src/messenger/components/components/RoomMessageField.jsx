453604581a037648fe00e693e73ebfb6771bd709
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-21 00:39:06 +0300

    29612

--- client/src/messenger/components/components/RoomMessageField.jsx
+++ client/src/messenger/components/components/RoomMessageField.jsx
@@ -99,7 +99,8 @@ export default function RoomMessageField() {
     } else if (files.videoFiles.length === limits.videos) {
       setCanChooseVideo(false);
     } else {
-      clearPreviews();
+      setCanChooseImage(true);
+      setCanChooseVideo(true);
     }
   }, [files, fileTypes, clearPreviews, slicePreviews]);
 
