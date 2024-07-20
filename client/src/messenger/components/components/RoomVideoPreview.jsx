e73c369ec56d2b814b49598202b4e87cc5cec4c0
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-20 15:02:03 +0300

    10969

--- client/src/messenger/components/components/RoomVideoPreview.jsx
+++ client/src/messenger/components/components/RoomVideoPreview.jsx
@@ -4,18 +4,19 @@ import Times from "./Times";
 export default function RoomImagePreview({ url, deletePreview }) {
   const { handleLoaded } = useWindow();
   return (
-    <video
-      controls
-      width="200"
-      src={url}
-      onLoadedData={handleLoaded}
-      className="room-preview"
-    >
+    <div className="room-preview-wrapper">
+      <video
+        controls
+        width="200"
+        src={url}
+        onLoadedData={handleLoaded}
+        className="room-preview"
+      />
       <Times
         onClick={deletePreview}
         className="room-preview-delete"
         item={url}
       />
-    </video>
+    </div>
   );
 }
