913e140f9a561e46e8fa1bad327ff0891cab20ec
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-20 20:01:13 +0300

    11972

--- client/src/messenger/data/errors.js
+++ client/src/messenger/data/errors.js
@@ -1,6 +1,6 @@
 import { limits } from "./messengerConfiguration";
 
 export const errors = {
-  imagesCount: `Не загружайте больше ${limits.images} изображений`,
-  videosCount: `Не загружайте больше ${limits.videos} видео`,
+  imagesCount: `Ошибка: Нельзя загружать больше ${limits.images} изображений`,
+  videosCount: `Ошибка: Нельзя загружать больше ${limits.videos} видео`,
 };
