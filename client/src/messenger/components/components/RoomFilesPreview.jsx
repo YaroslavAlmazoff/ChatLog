0579672d82cd9e468e8b1446627a36cb86f7bd33
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-20 14:53:41 +0300

    17426

--- client/src/messenger/components/components/RoomFilesPreview.jsx
+++ client/src/messenger/components/components/RoomFilesPreview.jsx
@@ -1,15 +1,26 @@
 import RoomImagePreview from "./RoomImagePreview";
-import RoomVideoPreview from "./RoomImagePreview";
+import RoomVideoPreview from "./RoomVideoPreview";
 import "../../styles/RoomPreview.css";
 
-export default function RoomFilesPreview({ files, filesVisible }) {
+export default function RoomFilesPreview({ files, setFiles, filesVisible }) {
+  const filterPreviews = (filterable, url) =>
+    [...filterable].filter((item) => item.url !== url);
+
+  const deletePreview = (url) => {
+    setFiles((prev) => ({
+      ...prev,
+      ...(url.includes("image")
+        ? { imageFiles: filterPreviews(prev.imageFiles, url) }
+        : { videoFiles: filterPreviews(prev.videoFiles, url) }),
+    }));
+  };
   return filesVisible ? (
     <div className="message-selected-files">
       {files.imageFiles.map((file) => (
-        <RoomImagePreview url={file.url} />
+        <RoomImagePreview url={file.url} deletePreview={deletePreview} />
       ))}
       {files.videoFiles.map((file) => (
-        <RoomVideoPreview url={file.url} />
+        <RoomVideoPreview url={file.url} deletePreview={deletePreview} />
       ))}
     </div>
   ) : (
--- client/src/messenger/components/components/RoomImagePreview.jsx
+++ client/src/messenger/components/components/RoomImagePreview.jsx
@@ -1,6 +1,7 @@
 import useWindow from "../../hooks/useWindow";
+import Times from "./Times";
 
-export default function RoomVideoPreview({ url }) {
+export default function RoomVideoPreview({ url, deletePreview }) {
   const { handleLoaded } = useWindow();
   return (
     <img
@@ -8,6 +9,12 @@ export default function RoomVideoPreview({ url }) {
       alt="uploaded"
       onLoadedData={handleLoaded}
       className="room-preview"
-    />
+    >
+      <Times
+        onClick={deletePreview}
+        className="room-preview-delete"
+        item={url}
+      />
+    </img>
   );
 }
