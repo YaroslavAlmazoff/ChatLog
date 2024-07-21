ad5ed9d6b13cf2b2de070969fd002d9f44b816fb
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-21 15:24:00 +0300

    6876

--- client/src/messenger/components/components/RoomFilesPreview.jsx
+++ client/src/messenger/components/components/RoomFilesPreview.jsx
@@ -1,12 +1,17 @@
 import RoomImagePreview from "./RoomImagePreview";
 import RoomVideoPreview from "./RoomVideoPreview";
 import "../../styles/RoomPreview.css";
+import { useEffect } from "react";
 
 export default function RoomFilesPreview({
   files,
   filesVisible,
   deletePreview,
 }) {
+  useEffect(() => {
+    console.log(files);
+  }, [files]);
+
   return filesVisible ? (
     <div className="message-selected-files">
       {files.imageFiles.map((file) => (
