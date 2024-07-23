843b3c254bf60aa040953001cfc2418c9306755e
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-23 22:19:59 +0300

    32392

--- client/src/messenger/components/components/RoomMessageActions.jsx
+++ client/src/messenger/components/components/RoomMessageActions.jsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import deleteIcon from "../../img/delete.png";
 import editIcon from "../../img/edit.png";
 import replyIcon from "../../img/reply.png";
 import shareIcon from "../../img/share.png";
 
-export default function RoomMessageActions({ message }) {
+export default function RoomMessageActions({ message, showActions }) {
   const { userId } = useContext(AuthContext);
   const isMyMessage = message.user === userId;
+
+  const [animation, setAnimation] = useState(null);
 
   const messageActions = [
     { icon: deleteIcon, available: isMyMessage, onClick: () => {} },
@@ -16,8 +18,18 @@ export default function RoomMessageActions({ message }) {
     { icon: shareIcon, available: true, onClick: () => {} },
   ];
 
+  useEffect(() => {
+    if (showActions) {
+      setAnimation("room-message-actions-show");
+    } else {
+      setAnimation("room-message-actions-hide");
+    }
+
+    return () => setAnimation(null);
+  }, [showActions]);
+
   return (
-    <div className="room-message-actions">
+    <div className={`room-message-actions ${animation}`}>
       <span className="room-message-date">{message.date}</span>
       <div className="room-message-actions-list">
         {messageActions.map((action) => (
