5beda06e20fda00a69e262145a3345645c6dca3e
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-23 23:14:38 +0300

    25590

--- client/src/messenger/components/components/RoomMessage.jsx
+++ client/src/messenger/components/components/RoomMessage.jsx
@@ -1,42 +1,22 @@
 import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import useFile from "../../hooks/useFile";
 import RoomMessageActions from "./RoomMessageActions";
 import RoomMessageImages from "./RoomMessageImages";
 import RoomMessageVideos from "./RoomMessageVideos";
-import { actionsAnimationClasses } from "../../data/messengerConfiguration";
 import "../../styles/RoomMessage.css";
 
 export default function RoomMessage({ message }) {
   const { fileFromServer } = useFile();
-  const [actionsVisibility, setActionsVisibility] = useState(false);
-  const [animation, setAnimation] = useState(null);
-
-  const showActions = () => {
-    setActionsVisibility(true);
-    setAnimation(actionsAnimationClasses.show);
-  };
-  const hideActions = () => {
-    setAnimation(actionsAnimationClasses.hide);
-    setTimeout(() => {
-      setActionsVisibility(false);
-    }, 500);
-  };
-
-  const toggleActionsVisibility = () => {
-    if (animation === actionsAnimationClasses.show) {
-      showActions();
-    } else {
-      hideActions();
-    }
-  };
+  const [showActions, setShowActions] = useState(false);
 
   return (
     <div className="room-message-wrapper">
       <div
         className="room-message"
-        onMouseOver={showActions}
-        onMouseOut={hideActions}
-        onClick={toggleActionsVisibility}
+        onMouseOver={() => setShowActions(true)}
+        onMouseOut={() => setShowActions(false)}
+        onClick={() => setShowActions((prev) => !prev)}
       >
         <div className="room-message-top">
           <img
@@ -50,9 +30,14 @@ export default function RoomMessage({ message }) {
         <RoomMessageImages images={message.images} />
         <RoomMessageVideos videos={message.videos} />
       </div>
-      {actionsVisibility && (
-        <RoomMessageActions message={message} animation={animation} />
-      )}
+      <CSSTransition
+        in={showActions}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <RoomMessageActions message={message} />
+      </CSSTransition>
     </div>
   );
 }
