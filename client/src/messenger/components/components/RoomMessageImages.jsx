56a630974783304175cd218006f465a6546f110a
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-24 12:57:19 +0300

    16787

--- client/src/messenger/components/components/RoomMessageImages.jsx
+++ client/src/messenger/components/components/RoomMessageImages.jsx
@@ -1,15 +1,20 @@
 import useFile from "../../hooks/useFile";
 import { folders } from "../../data/messengerConfiguration";
+import { useNavigate } from "react-router";
 
 export default function RoomMessageImages({ images }) {
+  const navigate = useNavigate();
   const { fileFromServer } = useFile();
+
+  const file = fileFromServer(folders.images, images);
 
   return (
     <div className="room-message-images">
       {images ? (
         images.map((image, index) => (
           <img
-            src={fileFromServer(folders.images, image)}
+            onClick={() => navigate(file)}
+            src={file}
             alt={image}
             className={`room-message-image${
               index !== images.length - 1 ? " room-message-media-margin" : ""
