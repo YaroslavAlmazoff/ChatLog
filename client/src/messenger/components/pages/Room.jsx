bb9cb4a23d8b9a09dcf71c0078451e90cb2ee31e
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-25 14:32:32 +0300

    25828

--- client/src/messenger/components/pages/Room.jsx
+++ client/src/messenger/components/pages/Room.jsx
@@ -1,17 +1,15 @@
 import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
 import RoomHead from "../components/RoomHead";
 import RoomMessageField from "../components/RoomMessageField";
 import RoomMessages from "../components/RoomMessages";
 import useAPI from "../../hooks/useAPI";
-import "../../styles/Room.css";
-import { useParams } from "react-router";
 import useFile from "../../hooks/useFile";
 import { AuthContext } from "../../../context/AuthContext";
-import useVerify from "../../../common_hooks/verify.hook";
+import "../../styles/Room.css";
 
 export default function Room() {
   const { id } = useParams();
-  //const { verify } = useVerify();
   const { getRoom } = useAPI();
   const { fileFromServer } = useFile();
 
@@ -23,6 +21,18 @@ export default function Room() {
     bg: "",
   });
   const [messages, setMessages] = useState([
+    {
+      message: "Привет",
+      user: "628e5aab0153706a3e18fe79",
+      name: "Yaroslav Almazoff",
+      date: "23.07.2024 12:00",
+      avatar: "907d4938-52fa-4c48-a421-6245c7f2d453.jpg",
+      images: [
+        "37d5055a-aea0-4397-b640-6d06b8d8a497.jpg",
+        "3ff98630-d038-4093-878f-69232741e273.jpg",
+      ],
+      videos: ["video1.mp4", "video2.mp4"],
+    },
     {
       message: "Привет",
       user: "628e5aab0153706a3e18fe79",
