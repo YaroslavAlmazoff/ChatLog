327954c783db91c7dd43153f017de066be0b948c
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-27 15:03:29 +0300

    322

--- client/src/messenger/components/components/RoomMessages.jsx
+++ client/src/messenger/components/components/RoomMessages.jsx
@@ -6,9 +6,18 @@ export default function RoomMessages({ messages }) {
 
   useEffect(() => {
     if (messagesListRef.current) {
+      console.log(
+        messagesListRef.current.scrollTop,
+        messagesListRef.current.scrollHeight
+      );
       messagesListRef.current.scrollTo(0, messagesListRef.current.scrollHeight);
+      messagesListRef.current.scrollTop = messagesListRef.current.scrollHeight;
+      console.log(
+        messagesListRef.current.scrollTop,
+        messagesListRef.current.scrollHeight
+      );
     }
-  }, []);
+  }, [messages]);
 
   return (
     <div className="room-messages" ref={messagesListRef}>
