900264b8fb844c830d4bb17253a92d51d5080fad
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-27 20:53:14 +0300

    31172

--- client/src/messenger/components/components/RoomMessages.jsx
+++ client/src/messenger/components/components/RoomMessages.jsx
@@ -3,16 +3,6 @@ import ScrollableFeed from "react-scrollable-feed";
 import RoomMessage from "./RoomMessage";
 
 export default function RoomMessages({ messages }) {
-  const messagesEndRef = useRef(null);
-
-  const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
-
   return (
     <div className="room-messages">
       <ScrollableFeed>
