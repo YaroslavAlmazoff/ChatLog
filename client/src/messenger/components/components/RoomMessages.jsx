f171d03e1bf4a4eb29155c23e0141de812adcdf3
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-27 16:13:05 +0300

    31934

--- client/src/messenger/components/components/RoomMessages.jsx
+++ client/src/messenger/components/components/RoomMessages.jsx
@@ -1,14 +1,25 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import RoomMessage from "./RoomMessage";
 
 export default function RoomMessages({ messages }) {
   const messagesListRef = useRef(null);
+  const [scrollBottom, setScrollBottom] = useState(false);
 
-  useLayoutEffect(() => {
-    if (messagesListRef.current) {
+  useEffect(() => {
+    if (scrollBottom && messagesListRef.current) {
       messagesListRef.current.scrollTop = messagesListRef.current.scrollHeight;
+      setScrollBottom(false);
     }
-  }, [messages]);
+  }, [messages, scrollBottom]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScrollBottom(true);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="room-messages" ref={messagesListRef}>
