58454a5ec9922123d4f1430f747b0f4b84097110
yaroslav <yaroslav.almazoff@mail.ru>
2024-07-29 22:48:13 +0300

    11222

--- client/src/messenger/components/pages/Room.jsx
+++ client/src/messenger/components/pages/Room.jsx
@@ -53,7 +53,6 @@ export default function Room() {
     const startEventSource = () => {
       const eventSource = createEventSource(id);
       eventSource.onmessage = function (event) {
-        console.log("дата ебать");
         const messagesData = JSON.parse(event.data);
         setMessages((prev) => [...prev, messagesData.messages]);
         console.log(messagesData.messages);
