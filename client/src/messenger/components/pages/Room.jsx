e3e57184fb343053512503c83bfbf49f29680add
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-21 16:24:49 +0300

    5670

--- client/src/messenger/components/pages/Room.jsx
+++ client/src/messenger/components/pages/Room.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useMemo } from "react";
+import { useEffect, useState, useRef, useMemo, useContext } from "react";
 import { useParams } from "react-router";
 import RoomHead from "../components/RoomHead";
 import RoomMessageField from "../components/RoomMessageField";
@@ -7,11 +7,13 @@ import useAPI from "../../hooks/useAPI";
 import useFile from "../../hooks/useFile";
 import { messagesDataTypes } from "../../data/messengerConfiguration";
 import "../../styles/Room.css";
+import { AuthContext } from "../../../context/AuthContext";
 
 export default function Room() {
   const params = useParams();
   const { getRoom, createEventSource, getMessages } = useAPI();
   const { fileFromServer } = useFile();
+  const { userId } = useContext(AuthContext);
 
   const feedRef = useRef(null);
 
@@ -39,23 +41,28 @@ export default function Room() {
         const isCreate = messagesData.type === messagesDataTypes.create;
         const isLoad = messagesData.type === messagesDataTypes.load;
 
-        if (isCreate) {
-          setMessages((prev) =>
-            isInit || isLoad
-              ? [...newMessages, ...prev]
-              : [...prev, ...newMessages]
-          );
+        const isMyMessage = messagesData.user === userId;
 
-          if (feedRef.current) {
-            if (isInit) {
-              feedRef.current.scrollTop = feedRef.current.scrollHeight;
-            } else {
-              setTimeout(() => {
-                feedRef.current.scrollTop =
-                  feedRef.current.scrollHeight - currentHeight;
-              }, 0);
-            }
+        if (isCreate) {
+          setMessages((prev) => [...prev, ...newMessages]);
+        } else if ((isInit || isLoad) && isMyMessage) {
+          setMessages((prev) => [...newMessages, ...prev]);
+
+          if (isLoad && feedRef.current) {
+            setTimeout(() => {
+              feedRef.current.scrollTop =
+                feedRef.current.scrollHeight - currentHeight;
+            }, 0);
           }
+
+          setLoading(false);
+        }
+
+        if (!isLoad && isMyMessage) {
+          feedRef.current.scrollTop = feedRef.current.scrollHeight;
+        }
+
+        if (isCreate && isMyMessage) {
           setLoading(false);
         }
       };
@@ -63,7 +70,7 @@ export default function Room() {
 
     getData();
     startEventSource();
-  }, [createEventSource, getRoom, id]);
+  }, [createEventSource, getRoom, id, userId]);
 
   useEffect(() => {
     getMessages(page, offset);
