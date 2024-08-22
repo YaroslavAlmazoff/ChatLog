2d831eb5dfcc362dbdb4b714bc069837b8d352dd
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-22 12:43:45 +0300

    13050

--- client/src/messenger/components/components/RoomMessageImages.jsx
+++ client/src/messenger/components/components/RoomMessageImages.jsx
@@ -1,27 +1,11 @@
-import useFile from "../../hooks/useFile";
-import useWindow from "../../hooks/useWindow";
-import { folders } from "../../data/messengerConfiguration";
+import RoomMessageImage from "./RoomMessageImage";
 
 export default function RoomMessageImages({ images }) {
-  const { fileFromServer } = useFile();
-  const { openInNewTab } = useWindow();
-
   return (
     <div className="room-message-images">
       {images.length ? (
         images.map((image, index) => (
-          <img
-            onClick={() => openInNewTab(folders.images, image)}
-            src={fileFromServer(folders.images, image)}
-            alt={image}
-            className={`${
-              images.length === 1
-                ? "room-message-single-image"
-                : "room-message-image"
-            }${
-              index !== images.length - 1 ? " room-message-media-margin" : ""
-            }`}
-          />
+          <RoomMessageImage image={image} index={index} count={images.length} />
         ))
       ) : (
         <></>
