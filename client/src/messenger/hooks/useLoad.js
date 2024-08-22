7a73150234ce39ac308db193b62dbc6569a86e9f
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-22 16:08:49 +0300

    25934

--- client/src/messenger/hooks/useLoad.js
+++ client/src/messenger/hooks/useLoad.js
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function useLoad() {
   const [totalImages, setTotalImages] = useState(0);
   const [loadedImages, setLoadedImages] = useState(0);
 
-  const registerImage = () => {
+  const registerImage = useCallback(() => {
     console.log("Image registered");
     setTotalImages((prevTotal) => prevTotal + 1);
-  };
+  }, []);
 
-  const handleImageLoad = () => {
+  const handleImageLoad = useCallback(() => {
     console.log("Image loaded");
     setLoadedImages((prevLoaded) => prevLoaded + 1);
-  };
+  }, []);
 
   const allImagesLoaded = totalImages === 0 || loadedImages === totalImages;
   return { allImagesLoaded, registerImage, handleImageLoad };
