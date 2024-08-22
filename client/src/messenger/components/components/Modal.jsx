2c7cd4d8b11b0d9204e069f83321e86c0779b758
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-22 21:30:08 +0300

    23068

--- client/src/messenger/components/components/Modal.jsx
+++ client/src/messenger/components/components/Modal.jsx
@@ -5,8 +5,11 @@ const Modal = ({ show, onClose, children, type }) => {
   if (!show) return null;
 
   return (
-    <div className={`room-modal room-modal-${type}`} onClick={onClose}>
-      <div className="room-modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className="room-modal" onClick={onClose}>
+      <div
+        className={`room-modal-content room-modal-${type}`}
+        onClick={(e) => e.stopPropagation()}
+      >
         <span className="room-modal-close" onClick={onClose}>
           &times;
         </span>
