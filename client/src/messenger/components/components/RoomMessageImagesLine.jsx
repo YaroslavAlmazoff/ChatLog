f7e7ac497a37f45601e4a0071a2f7dd54ef55807
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-23 07:52:34 +0300

    14186

--- client/src/messenger/components/components/RoomMessageImagesLine.jsx
+++ client/src/messenger/components/components/RoomMessageImagesLine.jsx
@@ -3,7 +3,13 @@ import RoomMessageImage from "./RoomMessageImage";
 export default function RoomMessageImagesLine({ imagesLine }) {
   if (!imagesLine.length) return null;
   return (
-    <div className="room-message-images">
+    <div
+      className={`room-message-images${
+        imagesLine.length === 1
+          ? " room-message-images-height-single"
+          : " room-message-images-height"
+      }`}
+    >
       {imagesLine.map((image, index) => (
         <RoomMessageImage
           image={image}
