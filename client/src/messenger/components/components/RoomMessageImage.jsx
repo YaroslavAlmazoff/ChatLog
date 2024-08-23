b56ec81bbc2dd529c6c82745841611e3723813ec
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-23 08:17:09 +0300

    14381

--- client/src/messenger/components/components/RoomMessageImage.jsx
+++ client/src/messenger/components/components/RoomMessageImage.jsx
@@ -1,29 +1,41 @@
 import useFile from "../../hooks/useFile";
 import useWindow from "../../hooks/useWindow";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useContext } from "react";
 import { folders } from "../../data/messengerConfiguration";
 import { ImageLoadContext } from "../../context/ImageLoadContext";
+import useImage from "../../hooks/useImage";
 
 const RoomMessageImage = ({ image, index, count }) => {
   const { fileFromServer } = useFile();
   const { openInNewTab } = useWindow();
+  const { determineImageFormat } = useImage();
 
   const { registerImage, handleImageLoad } = useContext(ImageLoadContext);
+
+  const imageRef = useRef(null);
 
   useEffect(() => {
     registerImage();
   }, [registerImage]);
+
+  const onImageLoad = () => {
+    imageRef.current.classList.add(
+      `room-message-single-image-${determineImageFormat(imageRef.current)}`
+    );
+    handleImageLoad();
+  };
 
   return (
     <img
       onClick={() => openInNewTab(folders.images, image)}
       src={fileFromServer(folders.images, image)}
       alt={image}
-      onLoad={handleImageLoad}
+      ref={imageRef}
+      onLoad={onImageLoad}
       onError={handleImageLoad}
       className={`room-message-image${
-        count === 1 ? " room-message-single-image" : ""
+        count === 1 ? ` room-message-single-image` : ""
       }${index !== count - 1 ? " room-message-media-margin" : ""}`}
     />
   );
