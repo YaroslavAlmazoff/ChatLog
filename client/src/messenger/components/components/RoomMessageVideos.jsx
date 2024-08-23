1b2845e8dce12b04b90e7edebb22d9963bf87b02
yaroslav <yaroslav.almazoff@mail.ru>
2024-08-23 14:21:45 +0300

    21186

--- client/src/messenger/components/components/RoomMessageVideos.jsx
+++ client/src/messenger/components/components/RoomMessageVideos.jsx
@@ -1,14 +1,24 @@
+import { videosInLine } from "../../data/messengerConfiguration";
 import RoomMessageVideosLine from "./RoomMessageVideosLine";
 
 export default function RoomMessageVideos({ videos }) {
   const length = videos.length;
   if (!length) return null;
 
-  const videosLine = videos.slice(0, length === 2 ? 2 : length);
+  const lines = [];
+
+  for (let i = 0; i < length / videosInLine; i++) {
+    lines.push(videos.slice(i * videosInLine, (i + 1) * videosInLine));
+  }
+
+  //const videosLine = videos.slice(0, length === 2 ? 2 : length);
 
   return (
     <>
-      <RoomMessageVideosLine videosLine={videosLine} />
+      {lines.map((line) => (
+        <RoomMessageVideosLine videosLine={line} />
+      ))}
+      {/* <RoomMessageVideosLine videosLine={videosLine} /> */}
     </>
   );
 }
--- client/src/messenger/components/components/RoomMessageVideosLine.jsx
+++ client/src/messenger/components/components/RoomMessageVideosLine.jsx
@@ -3,6 +3,7 @@ import RoomMessageVideo from "./RoomMessageVideo";
 export default function RoomMessageVideosLine({ videosLine }) {
   const length = videosLine.length;
   if (!length) return null;
+
   return (
     <div className="room-message-videos">
       {videosLine.map((video, index) => (
