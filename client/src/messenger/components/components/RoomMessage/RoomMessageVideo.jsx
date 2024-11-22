6e08d4f0986380f5dc3074f5b7630d16a11ec6d5
yaroslav <yaroslav.almazoff@mail.ru>
2024-11-22 21:16:11 +0300

    22077

--- client/src/messenger/components/components/RoomMessage/RoomMessageVideo.jsx
+++ client/src/messenger/components/components/RoomMessage/RoomMessageVideo.jsx
@@ -8,7 +8,7 @@ import { MessageContext } from "../../../context/MessageContext";
 import useImage from "../../../hooks/useImage";
 
 export default function RoomMessageVideo({ video, index, count }) {
-  const { getIsLast } = useList();
+  const { getIsLast, getIsSingle } = useList();
 
   const { fileFromServer } = useFile();
   const { openInNewTab } = useWindow();
@@ -17,8 +17,10 @@ export default function RoomMessageVideo({ video, index, count }) {
   const { registerMedia, loadMedia, setJustSentMediaLoaded } =
     useContext(ImageLoadContext);
   const { message } = useContext(MessageContext);
+  const videoRef = useRef(null);
 
   const isLast = getIsLast(index, count);
+  const isSingle = getIsSingle(count);
 
   useEffect(() => {
     registerMedia();
@@ -26,8 +28,8 @@ export default function RoomMessageVideo({ video, index, count }) {
 
   const onVideoLoad = (e) => {
     if (isSingle) {
-      imageRef.current.classList.add(
-        `room-message-single-video-${determineImageFormat(imageRef.current)}`
+      videoRef.current.classList.add(
+        `room-message-single-video-${determineImageFormat(videoRef.current)}`
       );
     }
     if (message.isNew) {
