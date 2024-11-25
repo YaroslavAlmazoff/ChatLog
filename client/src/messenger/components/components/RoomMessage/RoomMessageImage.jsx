ca42bdcc2378449d3989ec96156bd5a3b0dbf3a6
yaroslav <yaroslav.almazoff@mail.ru>
2024-11-25 14:54:24 +0300

    16554

--- client/src/messenger/components/components/RoomMessage/RoomMessageImage.jsx
+++ client/src/messenger/components/components/RoomMessage/RoomMessageImage.jsx
@@ -22,7 +22,9 @@ export default function RoomMessageImage({ image, index, count }) {
   const isLast = getIsLast(index, count);
 
   useEffect(() => {
-    registerMedia();
+    if (message.isNew) {
+      registerMedia();
+    }
   }, [registerMedia]);
 
   const onImageLoad = (e) => {
