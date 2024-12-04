ccb821d26a4655d1b94897073bda03923f720878
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-04 21:14:35 +0300

    1236

--- client/src/common_hooks/observer.hook.js
+++ client/src/common_hooks/observer.hook.js
@@ -2,21 +2,31 @@ import { useEffect, useRef } from "react";
 
 export const useObserver = (ref, canLoad, loading, callback) => {
   const observer = useRef();
+  const isFirstRender = useRef(true); // Флаг первого рендера
 
   useEffect(() => {
     if (loading) return;
     if (observer.current) observer.current.disconnect();
 
-    var cb = function (entries) {
+    const cb = function (entries) {
       if (entries[0].isIntersecting && canLoad) {
+        if (isFirstRender.current) {
+          // Пропускаем первое пересечение
+          isFirstRender.current = false;
+          return;
+        }
         callback();
       }
     };
     const options = {
-      root: ref.current.parentElement,
+      root: ref.current?.parentElement,
     };
     observer.current = new IntersectionObserver(cb, options);
     observer.current.observe(ref.current);
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading]);
 };
