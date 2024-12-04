d3f721f642fe6648757dcb6e651224e77eec1704
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-04 21:23:42 +0300

    3576

--- client/src/messenger/components/pages/Room.jsx
+++ client/src/messenger/components/pages/Room.jsx
@@ -135,6 +135,10 @@ export default function Room() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, getMessages]);
 
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
+
   return (
     <div
       className="block room-size room"
