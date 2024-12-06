d791e40841460cc73e590f64fc605db7b525f48a
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-06 12:44:15 +0300

    17333

--- client/src/messenger/hooks/useScroll.js
+++ client/src/messenger/hooks/useScroll.js
@@ -1,9 +1,16 @@
 export default function useScroll() {
+  // const loadScroll = (ref, scroll) => {
+  //   setTimeout(() => {
+  //     const _fc = ref.current.offsetHeight;
+  //     ref.current.scrollTop = ref.current.scrollHeight - scroll;
+  //   }, 100);
+  // };
+
   const loadScroll = (ref, scroll) => {
-    setTimeout(() => {
+    requestAnimationFrame(() => {
       const _fc = ref.current.offsetHeight;
       ref.current.scrollTop = ref.current.scrollHeight - scroll;
-    }, 100);
+    });
   };
 
   const scrollToBottom = (ref) => {
