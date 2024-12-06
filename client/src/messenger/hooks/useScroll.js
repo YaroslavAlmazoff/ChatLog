7803fd0203b9a6668052b75b9248972b0ac48226
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-06 13:01:54 +0300

    11399

--- client/src/messenger/hooks/useScroll.js
+++ client/src/messenger/hooks/useScroll.js
@@ -1,36 +1,9 @@
-import { useEffect } from "react";
-
-const useMutationObserver = (ref, callback) => {
-  useEffect(() => {
-    if (!ref.current) return;
-
-    const observer = new MutationObserver(() => {
-      callback();
-    });
-
-    observer.observe(ref.current, {
-      childList: true,
-      subtree: true,
-    });
-
-    return () => observer.disconnect();
-  }, [ref, callback]);
-};
-
 export default function useScroll() {
-  // const loadScroll = (ref, scroll) => {
-  //   setTimeout(() => {
-  //     const _fc = ref.current.offsetHeight;
-  //     ref.current.scrollTop = ref.current.scrollHeight - scroll;
-  //   }, 100);
-  // };
-
   const loadScroll = (ref, scroll) => {
-    useMutationObserver(ref, () => {
-      if (ref.current) {
-        ref.current.scrollTop = ref.current.scrollHeight - scroll;
-      }
-    });
+    setTimeout(() => {
+      const _fc = ref.current.offsetHeight;
+      ref.current.scrollTop = ref.current.scrollHeight - scroll;
+    }, 50);
   };
 
   const scrollToBottom = (ref) => {
