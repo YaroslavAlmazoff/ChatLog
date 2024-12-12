6d5bdc6051bc4be00a090c0f274f40526282393c
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-12 18:51:57 +0300

    22936

--- client/src/messenger/hooks/useLoad.js
+++ client/src/messenger/hooks/useLoad.js
@@ -18,6 +18,7 @@ export default function useLoad(onAllMessagesLoaded) {
   }, []);
 
   useEffect(() => {
+    console.log(registered, loaded, registered === loaded);
     if (loaded && registered === loaded) {
       onAllMessagesLoaded();
       reset();
