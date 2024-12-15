a72b329fa8f1f5c6be1ceab793577d51f2254ea4
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-15 18:29:34 +0300

    24095

--- mobile/routes/groupMessagesMobile.js
+++ mobile/routes/groupMessagesMobile.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 const auth = require("../../middleware/auth.middleware");
-const MessengerGroupServiceMobile = require("../../services/MessengerGroupServiceMobile");
+const MessengerGroupServiceMobile = require("../services/MessengerGroupServiceMobile");
 
 router.get("/getchatrooms-mobile", auth, (req, res) => {
   try {
