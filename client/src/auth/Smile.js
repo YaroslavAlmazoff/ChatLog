bc9ed1d0af9af14183af085f73ad82612b6a1327
yaroslav <yaroslav.almazoff@mail.ru>
2024-12-16 21:35:12 +0300

    17015

--- client/src/auth/Smile.js
+++ client/src/auth/Smile.js
@@ -1,11 +1,13 @@
-import '../styles/smiles.css'
+import "./styles/smiles.css";
 
-const Smile = ({el, addSmile}) => {
-    return (
-        <div className="smile-wrapper" onClick={() => addSmile(el.code)}>
-            <span role="img" className="smile-img">{el.code}</span>
-        </div>
-    )
-}
+const Smile = ({ el, addSmile }) => {
+  return (
+    <div className="smile-wrapper" onClick={() => addSmile(el.code)}>
+      <span role="img" className="smile-img">
+        {el.code}
+      </span>
+    </div>
+  );
+};
 
-export default Smile
+export default Smile;
