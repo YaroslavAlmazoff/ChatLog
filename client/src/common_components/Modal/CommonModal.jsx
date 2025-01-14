66f682c4224c6bfe7e394cd13035eb28d943fad5
yaroslav <yaroslav.almazoff@mail.ru>
2025-01-14 19:35:20 +0300

    6642

--- client/src/common_components/Modal/CommonModal.jsx
+++ client/src/common_components/Modal/CommonModal.jsx
@@ -1,43 +1,34 @@
-import { modalTypes } from "../../../data/messengerConfiguration";
-import RoomModalImage from "./RoomModalImage";
-import RoomModalVideo from "./RoomModalVideo";
 import "./CommonModal.css";
+
+const modalTypes = {
+  neutral: "neutral",
+  success: "success",
+  error: "error",
+  image: "image",
+  video: "video",
+};
 
 const CommonModal = ({
   show = false,
   onClose = () => {},
   children = <div></div>,
   type = modalTypes.neutral,
-  url = "",
   padding = "20px",
 }) => {
   if (!show) return null;
 
-  const isSuccess = type === modalTypes.success;
-  const isNeutral = type === modalTypes.neutral;
-  const isError = type === modalTypes.error;
-  const isImage = type === modalTypes.image;
-  const isVideo = type === modalTypes.video;
-
   return (
-    <div className="room-modal" onClick={onClose}>
-      <span className="room-modal-close" onClick={onClose}>
+    <div className="common-modal" onClick={onClose}>
+      <span className="common-modal-close" onClick={onClose}>
         &times;
       </span>
-      {isSuccess || isNeutral || isError ? (
-        <div
-          className={`room-modal-content room-modal-content-width room-modal-${type} block-${type}`}
-          style={{ padding }}
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className="room-modal-body">{children}</div>
-        </div>
-      ) : (
-        <div className="room-modal-media">
-          <RoomModalImage isImage={isImage} url={url} />
-          <RoomModalVideo isVideo={isVideo} url={url} />
-        </div>
-      )}
+      <div
+        className={`common-modal-content common-modal-content-width common-modal-${type} block-${type}`}
+        style={{ padding }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="common-modal-body">{children}</div>
+      </div>
     </div>
   );
 };
