e3d5329a05b18ca34c756b2b22d9b10053e65355
yaroslav <yaroslav.almazoff@mail.ru>
2025-01-22 06:27:22 +0300

    30783

--- client/src/auth/components/PhotoItem.js
+++ client/src/auth/components/PhotoItem.js
@@ -1,15 +1,21 @@
-const PhotoItem = ({ item, deletePhoto, isOwner }) => {
+import { useContext } from "react";
+import usePhotos from "../hooks/usePhotos";
+import { ProfileContext } from "../context/ProfileContext";
+
+const PhotoItem = ({ photo }) => {
+  const { deletePhoto } = usePhotos();
+  const { isOwner } = useContext(ProfileContext);
   return (
     <div className="foto-div">
       <img
         className="user-foto-preview block"
         alt=""
-        src={process.env.REACT_APP_API_URL + "/userfotos/" + item.imageUrl}
+        src={process.env.REACT_APP_API_URL + "/userfotos/" + photo.imageUrl}
       />
       {isOwner && (
         <span
           className="delete-foto"
-          onClick={() => deletePhoto(item.imageUrl)}
+          onClick={() => deletePhoto(photo.imageUrl)}
         >
           Удалить
         </span>
--- client/src/auth/components/UserRightSide.js
+++ client/src/auth/components/UserRightSide.js
@@ -1,24 +1,20 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import useRandom from "../../common_hooks/random.hook";
 import "../styles/user.css";
 import PhotoItem from "./PhotoItem";
 import usePhotos from "../hooks/usePhotos";
+import { ProfileContext } from "../context/ProfileContext";
 
-const UserRightSide = ({ photos, isOwner }) => {
+const UserRightSide = () => {
   const { randomKey } = useRandom();
-  const { deletePhoto } = usePhotos();
+  const { photos } = useContext(ProfileContext);
 
   return (
     <div className="user-right-side">
       <div className="user-fotos">
         <p className="user-fotos-title">Фотографии {photos.length}</p>
         {photos.map((el) => (
-          <PhotoItem
-            key={randomKey()}
-            item={el}
-            deletePhoto={deletePhoto}
-            isOwner={isOwner}
-          />
+          <PhotoItem key={randomKey()} photo={el} />
         ))}
       </div>
     </div>
