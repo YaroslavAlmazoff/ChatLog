356b2faaea858197fdeb1d6da3a71781a63e9dbf
yaroslav <yaroslav.almazoff@mail.ru>
2026-01-25 17:13:38 +0300

    3960

---
+++ client/src/course/content/Content.jsx
@@ -0,0 +1,11 @@
+import "../styles/content.css";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <span>Course Content</span>
+    </div>
+  );
+};
+
+export default Content;
