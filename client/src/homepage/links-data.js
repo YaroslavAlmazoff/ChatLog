020afe14f9fc4c9f9b836660257c0fd0925f8f5a
yaroslav <yaroslav.almazoff@mail.ru>
2026-01-31 12:03:22 +0300

    1971

--- client/src/homepage/links-data.js
+++ client/src/homepage/links-data.js
@@ -1,27 +1,21 @@
-export const list1 = (userId) => {
-  [
-    { name: "Моя страница", link: `/user/${userId}` },
-    { name: "Сообщения", link: "/messages" },
-    { name: "Приложения", link: "/apps" },
-    { name: "Люди", link: "/users" },
-    { name: "Группы", link: `/publics` },
-  ];
-};
+export const list1 = (userId) => [
+  { name: "Моя страница", link: `/user/${userId}` },
+  { name: "Сообщения", link: "/messages" },
+  { name: "Приложения", link: "/apps" },
+  { name: "Люди", link: "/users" },
+  { name: "Группы", link: `/publics` },
+];
 
-export const list2 = () => {
-  [
-    { name: "Chat Log Видеоблогер", link: `/videohost` },
-    { name: "Chat Log Cloud", link: `/cloud` },
-  ];
-};
+export const list2 = () => [
+  { name: "Chat Log Видеоблогер", link: `/videohost` },
+  { name: "Chat Log Cloud", link: `/cloud` },
+];
 
-export const list3 = (userId) => {
-  [
-    { name: "Моя страница", link: `/user/${userId}` },
-    { name: "Сообщения", link: "/messages" },
-    { name: "Игры", link: "/store" },
-    { name: "Люди", link: "/users" },
-    { name: "Группы", link: `/publics` },
-    { name: "Сервисы", link: "/services" },
-  ];
-};
+export const list3 = (userId) => [
+  { name: "Моя страница", link: `/user/${userId}` },
+  { name: "Сообщения", link: "/messages" },
+  { name: "Игры", link: "/store" },
+  { name: "Люди", link: "/users" },
+  { name: "Группы", link: `/publics` },
+  { name: "Сервисы", link: "/services" },
+];
