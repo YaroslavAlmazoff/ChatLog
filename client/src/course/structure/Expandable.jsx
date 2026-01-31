b6f30a301cba1bc1575ceb5cfdaa168a91a743a8
yaroslav <yaroslav.almazoff@mail.ru>
2026-01-31 11:42:08 +0300

    32012

--- client/src/course/structure/Expandable.jsx
+++ client/src/course/structure/Expandable.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Expandable({
   title,
   children,
--- client/src/homepage/components/HomePageNavigation.js
+++ client/src/homepage/components/HomePageNavigation.js
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import CourseStructure from "../../course/CourseStructure";
 import { list1, list2, list3 } from "../links-data";
+import course from "../../course/course.json";
 
 const HomePageNavigation = () => {
   const auth = useContext(AuthContext);
