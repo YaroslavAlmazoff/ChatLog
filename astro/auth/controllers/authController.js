3e297cb95147c6b641c4fa935873d746281bed88
yaroslav <yaroslav.almazoff@mail.ru>
2026-08-29 16:30:32 +0300

    31255

--- astro/auth/controllers/authController.js
+++ astro/auth/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const User = require("../models/AstroUser");
